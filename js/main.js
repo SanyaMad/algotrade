ab277501218606a7df34bd2eda91204f9e4a7b51
SanyaMad <90633231+SanyaMad@users.noreply.github.com>
2022-03-09 18:54:58 +0300

    update 09.03

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+      const secondSwiper = new Swiper('.swiper-second', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.lessons__swiper__button-next',
+          prevEl: '.lessons__swiper__button-prev',
+        },
+      });
+});
